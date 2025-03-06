3b39e8caabe2036d14499e10e1c974d57d3c86dc
AJoshQuinto <166543014+AJoshQuinto@users.noreply.github.com>
2025-03-05 17:21:01 -0800

    Removed the id=1 from url

---
+++ GroupFind/Data/Controllers/EventController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using GroupFind.Data.Models;
+using GroupFind.Data;
+
+public class EventController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public EventController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: Events
+    public async Task<IActionResult> Index(string search)
+    {
+        var events = from e in _context.Event.Include(e => e.Category).Include(e => e.User)
+                     select e;
+
+        if (!string.IsNullOrEmpty(search))
+        {
+            events = events.Where(e => e.EventTitle.Contains(search) || e.City.Contains(search) || e.Country.Contains(search));
+        }
+
+        ViewData["SearchQuery"] = search;
+        return View(await events.ToListAsync());
+    }
+
+    // GET: Events/Details/{eventTitle}
+    public async Task<IActionResult> Details(string eventTitle)
+    {
+        if (string.IsNullOrEmpty(eventTitle))
+        {
+            return NotFound();
+        }
+
+        var eventItem = await _context.Event
+            .Include(e => e.Category)
+            .Include(e => e.User)
+            .FirstOrDefaultAsync(m => m.EventTitle == eventTitle);
+
+        if (eventItem == null)
+        {
+            return NotFound();
+        }
+
+        return View(eventItem);
+    }
+}
